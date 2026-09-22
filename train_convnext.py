a2aef75a5c5efc9fa84f76cabec6a50b14a94508
Taybh <tayebehb92@gmail.com>
2026-09-22 16:00:10 +0200

    change the LR

--- train_convnext.py
+++ train_convnext.py
@@ -50,7 +50,7 @@ MODEL_INPUT_SIZE = 224     # aspect-ratio-preserving fit + padding
 BATCH_SIZE = 16
 NUM_WORKERS = 0
 TOTAL_EPOCHS = 50
-LEARNING_RATE = 1e-5
+LEARNING_RATE = 1e-4 
 WEIGHT_DECAY = 1e-4
 MIN_VAL_LOSS_DELTA = 1e-5
 
